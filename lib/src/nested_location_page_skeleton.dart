d2c54574798c2bfcca5c8c4d594d29183497e588
Tobias Marschall <tobias.marschall@online.de>
2022-10-25 10:33:52 +0200

    make builder optional

--- lib/src/nested_location_page_skeleton.dart
+++ lib/src/nested_location_page_skeleton.dart
@@ -3,21 +3,23 @@ import 'package:flutter/material.dart';
 import '../working_router.dart';
 
 class NestedLocationPageSkeleton<ID> extends LocationPageSkeleton<ID> {
-  final Widget Function(BuildContext context, Widget child) builder;
+  final Widget Function(BuildContext context, Widget child)? builder;
 
   NestedLocationPageSkeleton({
     required WorkingRouter<ID> router,
     required BuildPages<ID> buildPages,
-    required this.builder,
+    this.builder,
     super.buildPage,
     super.buildKey,
   }) : super(
           child: Builder(
             builder: (context) {
-              return builder(
-                context,
-                NestedRouting(router: router, buildPages: buildPages),
-              );
+              final nested =
+                  NestedRouting(router: router, buildPages: buildPages);
+              if (builder == null) {
+                return nested;
+              }
+              return builder(context, nested);
             },
           ),
         );
