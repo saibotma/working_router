0cd935abb701b9f99a430b1afcc1b1c9c0f207a0
saibotma <tobias.marschall@online.de>
2025-11-28 09:21:19 +0100

    Introduce LocationPageBuilderSkeleton

--- lib/src/location_page_skeleton.dart
+++ lib/src/location_page_skeleton.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import '../working_router.dart';
 import 'inherited_working_router_data.dart';
 
@@ -40,6 +41,55 @@ class LocationPageSkeleton<ID> {
             return false;
           },
           child: child,
+        ),
+      ),
+    );
+    final key = buildKey?.call(location) ?? ValueKey(location);
+    return LocationPage(
+      buildPage?.call(key, wrappedChild) ??
+          MaterialPage<dynamic>(key: key, child: wrappedChild),
+    );
+  }
+}
+
+class LocationPageBuilderSkeleton<ID> {
+  final Widget Function(BuildContext, WorkingRouterData<ID> data) buildChild;
+  final Page<dynamic> Function(LocalKey? key, Widget child)? buildPage;
+  final LocalKey Function(Location<ID> location)? buildKey;
+
+  LocationPageBuilderSkeleton({
+    required this.buildChild,
+    this.buildPage,
+    this.buildKey,
+  });
+
+  LocationPage inflate({
+    required WorkingRouter<ID> router,
+    required WorkingRouterData<ID> data,
+    required Location<ID> location,
+  }) {
+    final wrappedChild = InheritedWorkingRouterData(
+      // Require an extra inherited widget for each location, because
+      // the widget below should have access to old router data, when
+      // it animates out of view because of a routing event.
+      data: data,
+      child: NearestLocation<ID>(
+        location: location,
+        child: NotificationListener(
+          onNotification: (notification) {
+            if (notification is AddLocationGuardMessage) {
+              router.addGuard(notification.state);
+              return true;
+            }
+            if (notification is RemoveLocationGuardMessage) {
+              router.removeGuard(notification.state);
+              return true;
+            }
+            return false;
+          },
+          child: Builder(builder: (context) {
+            return buildChild(context, data);
+          }),
         ),
       ),
     );
