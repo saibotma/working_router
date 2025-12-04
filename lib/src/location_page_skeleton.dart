c1bf50728b5660036458b70d45980de41d0d607b
saibotma <tobias.marschall@online.de>
2025-12-04 16:56:35 +0100

    Set widget key automatically

--- lib/src/location_page_skeleton.dart
+++ lib/src/location_page_skeleton.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 import '../working_router.dart';
 import 'inherited_working_router_data.dart';
 
+typedef LocationChildBuilder<ID> = Widget Function(
+    BuildContext, WorkingRouterData<ID> data);
+typedef LocationPageBuilder = Page<dynamic> Function(
+    LocalKey? key, Widget child);
+typedef LocationPageKeyBuilder<ID> = LocalKey Function(Location<ID> location);
+
 abstract interface class BaseLocationPageSkeleton<ID> {
   LocationPage inflate({
     required WorkingRouter<ID> router,
@@ -12,64 +18,15 @@ abstract interface class BaseLocationPageSkeleton<ID> {
 }
 
 /// A blueprint used by the router to insert a page built by [buildPage]
-/// displaying [child] into a navigator.
+/// displaying [buildChild] into a navigator.
 ///
 /// The key built by [buildKey] gets passed to [buildPage].
 ///
 /// Does not hold state and thus can be reused.
-class LocationPageSkeleton<ID> implements BaseLocationPageSkeleton<ID> {
-  final Widget child;
-  final Page<dynamic> Function(LocalKey? key, Widget child)? buildPage;
-  final LocalKey Function(Location<ID> location)? buildKey;
-
-  LocationPageSkeleton({required this.child, this.buildPage, this.buildKey});
-
-  @override
-  LocationPage inflate({
-    required WorkingRouter<ID> router,
-    required WorkingRouterData<ID> data,
-    required Location<ID> location,
-  }) {
-    final wrappedChild = InheritedWorkingRouterData(
-      // Require an extra inherited widget for each location, because
-      // the widget below should have access to old router data, when
-      // it animates out of view because of a routing event.
-      data: data,
-      child: NearestLocation<ID>(
-        location: location,
-        child: NotificationListener(
-          onNotification: (notification) {
-            if (notification is AddLocationGuardMessage) {
-              router.addGuard(notification.state);
-              return true;
-            }
-            if (notification is RemoveLocationGuardMessage) {
-              router.removeGuard(notification.state);
-              return true;
-            }
-            return false;
-          },
-          child: child,
-        ),
-      ),
-    );
-    final key = buildKey?.call(location) ?? ValueKey(location);
-    return LocationPage(
-      buildPage?.call(key, wrappedChild) ??
-          MaterialPage<dynamic>(key: key, child: wrappedChild),
-    );
-  }
-}
-
-typedef LocationChildBuilder<ID> = Widget Function(
-    BuildContext, WorkingRouterData<ID> data);
-typedef LocationPageBuilder = Page<dynamic> Function(
-    LocalKey? key, Widget child);
-
 class LocationPageBuilderSkeleton<ID> implements BaseLocationPageSkeleton<ID> {
   final LocationChildBuilder<ID> buildChild;
   final LocationPageBuilder? buildPage;
-  final LocalKey Function(Location<ID> location)? buildKey;
+  final LocationPageKeyBuilder<ID>? buildKey;
 
   LocationPageBuilderSkeleton({
     required this.buildChild,
@@ -102,9 +59,10 @@ class LocationPageBuilderSkeleton<ID> implements BaseLocationPageSkeleton<ID> {
             }
             return false;
           },
-          child: Builder(builder: (context) {
-            return buildChild(context, data);
-          }),
+          child: Builder(
+            key: ValueKey(data.uri.path),
+            builder: (context) => buildChild(context, data),
+          ),
         ),
       ),
     );
@@ -114,4 +72,12 @@ class LocationPageBuilderSkeleton<ID> implements BaseLocationPageSkeleton<ID> {
           MaterialPage<dynamic>(key: key, child: wrappedChild),
     );
   }
+}
+
+class LocationPageSkeleton<ID> extends LocationPageBuilderSkeleton<ID> {
+  LocationPageSkeleton({
+    required Widget child,
+    super.buildPage,
+    super.buildKey,
+  }) : super(buildChild: (_, __) => child);
 }
--- lib/src/nested_location_page_skeleton.dart
+++ lib/src/nested_location_page_skeleton.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../working_router.dart';
 
-class NestedLocationPageSkeleton<ID> extends LocationPageSkeleton<ID> {
+class NestedLocationPageSkeleton<ID> extends LocationPageBuilderSkeleton<ID> {
   final Widget Function(BuildContext context, Widget child)? builder;
 
   NestedLocationPageSkeleton({
@@ -13,18 +13,16 @@ class NestedLocationPageSkeleton<ID> extends LocationPageSkeleton<ID> {
     super.buildKey,
     String? debugLabel,
   }) : super(
-          child: Builder(
-            builder: (context) {
-              final nested = NestedRouting(
-                router: router,
-                buildPages: buildPages,
-                debugLabel: debugLabel,
-              );
-              if (builder == null) {
-                return nested;
-              }
-              return builder(context, nested);
-            },
-          ),
+          buildChild: (context, _) {
+            final nested = NestedRouting(
+              router: router,
+              buildPages: buildPages,
+              debugLabel: debugLabel,
+            );
+            if (builder == null) {
+              return nested;
+            }
+            return builder(context, nested);
+          },
         );
 }
